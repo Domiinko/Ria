09b3a0b7e7de1a68a2474f7ff4b8e2a93ba00609
Domiinko <dominik.kmeto@student.ukf.sk>
2023-03-25 19:00:04 +0100

    projekt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { BodyComponent } from './body/body.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { KontaktyComponent } from './kontakty/kontakty.component';
+import { OnasComponent } from './onas/onas.component';
+import { LetiskoNitraComponent } from './letisko-nitra/letisko-nitra.component';
+import { LeteckaSkolaComponent } from './letecka-skola/letecka-skola.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    BodyComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    KontaktyComponent,
+    OnasComponent,
+    LetiskoNitraComponent,
+    LeteckaSkolaComponent
   ],
   imports: [
     BrowserModule,
